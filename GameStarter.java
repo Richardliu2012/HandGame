758a5616c0133295502a6962a1d5439ea8e7d46b
Richardliu2012 <Richardliu2012@gmail.com>
2016-01-14 17:07:06 +0800

    Update GameStarter.java

--- GameStarter.java
+++ GameStarter.java
@@ -5,7 +5,7 @@ import com.handgame.engine.BasicEngine;;
 
 public class GameStarter {
 
-	public void startGame(int mode, int clientchoice){
+	public String[] startGame(int mode, int clientchoice){
 		//Please do not change below block of codes, Initialize the Game
  		//-------------------------------------------------------------------
 		CommonService cs = new CommonService();
@@ -17,20 +17,21 @@ public class GameStarter {
 		
 		//Below codes start the game
 		//Mode 0 is Random Mode where computer will pick a number randomly
-                //Mode 1 is Advance Mode where computer will try to learn from client's previous gesture
+        //Mode 1 is Advance Mode where computer will try to learn from client's previous gesture
 		
 		String[] result;
 		cs.setClientchoice(clientchoice);
 		
 		
-	        result = (mode == 0) ? new BasicEngine().findResult(cs):new AdvanceEngine().findResult(cs);
-                System.out.println("You choose "+result[1]+" .");
-                System.out.println("Computer choose "+result[2]+" .");
-                System.out.println("You "+result[0]+" .");
+	    result = (mode == 0) ? new BasicEngine().findResult(cs):new AdvanceEngine().findResult(cs);
+        System.out.println("You choose "+result[1]+" .");
+        System.out.println("Computer choose "+result[2]+" .");
+        System.out.println("You "+result[0]+" .");
+	    return result;
 	}
 	
-	public static void main(String[] args){
-		GameStarter gs = new GameStarter();
-		gs.startGame(0,3);
-	}
+//	public static void main(String[] args){
+//		GameStarter gs = new GameStarter();
+//		gs.startGame(0,3);
+//	}
 }
